4f98707c0a23287f220229d6975a61c65cb96bb3
FajlaRabby24 <fajlarabbyla24@gmail.com>
2025-11-02 23:35:00 +0600

    video 6

---
+++ video6.js
@@ -0,0 +1,28 @@
+const postsArray = [
+  { id: "p-101", title: "Intro to SQL", author: "Alex" },
+  { id: "p-102", title: "Data Structures in JS", author: "Beth" },
+  { id: "p-103", title: "Understanding Reduce", author: "Chris" },
+  { id: "p-104", title: "CSS Grid Tricks", author: "Alex" },
+  { id: "p-105", title: "Intro to SQL", author: "Alex" },
+  { id: "p-106", title: "Data Structures in JS", author: "Beth" },
+  { id: "p-107", title: "Understanding Reduce", author: "Chris" },
+  { id: "p-108", title: "CSS Grid Tricks", author: "Alex" },
+];
+
+// const lookupTable = new Map();
+// postsArray.forEach((post, key) => lookupTable.set(post.id, post));
+
+const lookupTable = postsArray.reduce((table, post) => {
+  table[post.id] = post;
+  return table;
+}, {});
+
+// ---------- worst idea ----------
+console.time("key");
+// const post = postsArray.find((post) => post.id === "p-104");
+// ------- best idea
+const post = lookupTable["p-108"];
+console.timeEnd("key");
+
+console.log(post);
+// console.log(lookupTable);
